388f2899e705d3bd470a4003eb6974d7334ed86d
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-21 00:07:47 +0100

    Adds basic command structure

--- mann.py
+++ mann.py
@@ -1,2 +1,28 @@
 #!/usr/bin/env python
 """ mann: Simple, customisable quick-reference for shell commands """
+
+# pylint: disable-msg=E0401; (Undefined variable)
+
+import click
+
+@click.command()
+@click.option("--add", "-a", 'mode', flag_value="add")
+@click.option("--remove", "-r", 'mode', flag_value="remove")
+@click.argument("name")
+def main(mode, name):
+	if mode == "add":
+		add(name)
+	elif mode == "remove":
+		remove(name)
+
+	raise NotImplementedError()
+
+
+def add(name):
+	raise NotImplementedError()
+	exit()
+
+
+def remove(name):
+	raise NotImplementedError()
+	exit()
