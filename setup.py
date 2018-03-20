95240f647969c7881167ebb3aedd3c18d2cc5db0
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-21 00:08:41 +0100

    Adds basic setup.py for script installation

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='mann',
+    version='0.1',
+    py_modules=['mann'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        mann=mann:main
+    ''',
+)
